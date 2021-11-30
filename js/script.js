ca4847124712f6878153ead77a3bdc83605fa553
David Gordaniere <david.gordanier@gmail.comm>
2021-11-30 16:26:28 -0700

    Pseudo coding

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Click start button
+
+// prompt for character name
+
+// Enter character name
+
+// character name appears somewhere on screen
+
+// Elf graphic appears
+
+// display metrics (hunger, sleepiness, boredom, age) scale of 1-10
+
+// add buttons (feed, turn off lights, play)
+
+// increase (hunger, sleepiness, boredom,) metrics on interval
+
+// increase age every x minutes
+
+// morph at certain age
+
+// die if any metric hits 10
+
+// restart game
